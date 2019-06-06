08ae732a588c81c8e763e6e431689e9d8b256b80
Travis CI <travis@travis-ci.org>
2019-06-06 16:13:54 +0000

    Deploy code docs to GitHub Pages Travis build: 80
    Commit: faf1570d48bc610938fa50cb1bbad53a7ed06570

--- html/search/functions_b.js
+++ html/search/functions_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['menu',['menu',['../class_adafruit___arcada.html#ac67b46e3637df925bb9b06aca8befbce',1,'Adafruit_Arcada']]],
   ['mkdir',['mkdir',['../class_adafruit___arcada.html#a8f8e0af701cf758ac61a5412b4b5fb7d',1,'Adafruit_Arcada']]]
 ];
