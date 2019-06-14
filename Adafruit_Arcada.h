a28f59b00a8ff01c5ebc24a7bb44095b5f227ee8
brentru <robots199@me.com>
2019-06-14 12:41:46 -0400

    included wifinina, check for wifimodule, added wifi module check into examples

--- Adafruit_Arcada.h
+++ Adafruit_Arcada.h
@@ -215,45 +215,46 @@
   #define ARCADA_ACCEL_TYPE       ARCADA_ACCEL_LIS3DH
 
 #elif defined(ADAFRUIT_PYBADGE_AIRLIFT_M4)
+#include <WiFiNINA.h>
 
-  #define ARCADA_TFT_SPI         SPI1
-  #define ARCADA_TFT_CS          44       // Display CS Arduino pin number
-  #define ARCADA_TFT_DC          45       // Display D/C Arduino pin number
-  #define ARCADA_TFT_RST         46       // Display reset Arduino pin number
-  #define ARCADA_TFT_LITE        47
-  #define ARCADA_TFT_ROTATION     3
-  #define ARCADA_TFT_DEFAULTFILL  0xFFFF
-  #define ARCADA_TFT_INIT         init(320, 240)
-  #define ARCADA_TFT_TYPE         Adafruit_ST7789
-  #define ARCADA_TFT_WIDTH        320
-  #define ARCADA_TFT_HEIGHT       240
+#define ARCADA_TFT_SPI SPI1
+#define ARCADA_TFT_CS 44  // Display CS Arduino pin number
+#define ARCADA_TFT_DC 45  // Display D/C Arduino pin number
+#define ARCADA_TFT_RST 46 // Display reset Arduino pin number
+#define ARCADA_TFT_LITE 47
+#define ARCADA_TFT_ROTATION 3
+#define ARCADA_TFT_DEFAULTFILL 0xFFFF
+#define ARCADA_TFT_INIT init(320, 240)
+#define ARCADA_TFT_TYPE Adafruit_ST7789
+#define ARCADA_TFT_WIDTH 320
+#define ARCADA_TFT_HEIGHT 240
 
-  #define ARCADA_SPEAKER_ENABLE  51
-  #define ARCADA_NEOPIXEL_PIN     8
-  #define ARCADA_NEOPIXEL_NUM     5
-  #define ARCADA_AUDIO_OUT       A0
-  #define ARCADA_BUTTON_CLOCK    48
-  #define ARCADA_BUTTON_DATA     49
-  #define ARCADA_BUTTON_LATCH    50
-  #define ARCADA_BUTTON_SHIFTMASK_B           0x80
-  #define ARCADA_BUTTON_SHIFTMASK_A           0x40
-  #define ARCADA_BUTTON_SHIFTMASK_START       0x20
-  #define ARCADA_BUTTON_SHIFTMASK_SELECT      0x10
-  #define ARCADA_BUTTON_SHIFTMASK_LEFT        0x01
-  #define ARCADA_BUTTON_SHIFTMASK_UP          0x02
-  #define ARCADA_BUTTON_SHIFTMASK_DOWN        0x04
-  #define ARCADA_BUTTON_SHIFTMASK_RIGHT       0x08
+#define ARCADA_SPEAKER_ENABLE 51
+#define ARCADA_NEOPIXEL_PIN 8
+#define ARCADA_NEOPIXEL_NUM 5
+#define ARCADA_AUDIO_OUT A0
+#define ARCADA_BUTTON_CLOCK 48
+#define ARCADA_BUTTON_DATA 49
+#define ARCADA_BUTTON_LATCH 50
+#define ARCADA_BUTTON_SHIFTMASK_B 0x80
+#define ARCADA_BUTTON_SHIFTMASK_A 0x40
+#define ARCADA_BUTTON_SHIFTMASK_START 0x20
+#define ARCADA_BUTTON_SHIFTMASK_SELECT 0x10
+#define ARCADA_BUTTON_SHIFTMASK_LEFT 0x01
+#define ARCADA_BUTTON_SHIFTMASK_UP 0x02
+#define ARCADA_BUTTON_SHIFTMASK_DOWN 0x04
+#define ARCADA_BUTTON_SHIFTMASK_RIGHT 0x08
 
-  #define ARCADA_LIGHT_SENSOR                A7
-  #define ARCADA_BATTERY_SENSOR              A6
+#define ARCADA_LIGHT_SENSOR A7
+#define ARCADA_BATTERY_SENSOR A6
 
-  #define ARCADA_RIGHT_AUDIO_PIN             A0
-  #define ARCADA_LEFT_AUDIO_PIN              A1
+#define ARCADA_RIGHT_AUDIO_PIN A0
+#define ARCADA_LEFT_AUDIO_PIN A1
 
-  #define ARCADA_USE_JSON
-  #define ARCADA_USE_QSPI_FS
+#define ARCADA_USE_JSON
+#define ARCADA_USE_QSPI_FS
 
-  #define ARCADA_ACCEL_TYPE       ARCADA_ACCEL_LIS3DH
+#define ARCADA_ACCEL_TYPE ARCADA_ACCEL_LIS3DH
 #endif
 
 #if defined(ARCADA_USE_SD_FS)
@@ -370,6 +371,8 @@ class Adafruit_Arcada : public ARCADA_TFT_TYPE {
   Adafruit_LIS3DH accel = Adafruit_LIS3DH();
   bool hasAccel(void) { return _has_accel; }
 #endif
+
+  bool hasWiFi(void) { return _has_wifi; }
 
 #ifdef ARCADA_USE_JSON
   StaticJsonDocument<256> configJSON;  ///< The object to store our various settings, you need to restore/save this with (load/save)ConfigurationFile
