0efe769e35e03e6cd73cd14b296c870aca9affeb
Travis CI <travis@travis-ci.org>
2019-09-09 00:18:16 +0000

    Deploy code docs to GitHub Pages Travis build: 179
    Commit: b880080f695ca493d022ef524131141c9116f101

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['display',['display',['../class_adafruit___arcada___s_p_i_t_f_t.html#a88bcab129edcb9abf602037bff5945c0',1,'Adafruit_Arcada_SPITFT']]],
   ['displaybegin',['displayBegin',['../class_adafruit___arcada___s_p_i_t_f_t.html#af4957854a19443557e78f8a7e2f01c80',1,'Adafruit_Arcada_SPITFT']]],
-  ['drawbmp',['drawBMP',['../class_adafruit___arcada___s_p_i_t_f_t.html#a626072ad8d4e7a440c001efc649035ca',1,'Adafruit_Arcada_SPITFT']]]
+  ['drawbmp',['drawBMP',['../class_adafruit___arcada___s_p_i_t_f_t.html#a34b4252b54585e3c6c2e679395f50750',1,'Adafruit_Arcada_SPITFT']]]
 ];
--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['displaybegin',['displayBegin',['../class_adafruit___arcada___s_p_i_t_f_t.html#af4957854a19443557e78f8a7e2f01c80',1,'Adafruit_Arcada_SPITFT']]],
-  ['drawbmp',['drawBMP',['../class_adafruit___arcada___s_p_i_t_f_t.html#a626072ad8d4e7a440c001efc649035ca',1,'Adafruit_Arcada_SPITFT']]]
+  ['drawbmp',['drawBMP',['../class_adafruit___arcada___s_p_i_t_f_t.html#a34b4252b54585e3c6c2e679395f50750',1,'Adafruit_Arcada_SPITFT']]]
 ];
