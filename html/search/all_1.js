27fea978635fb4d559d8237ccba5f90b15ccc283
Travis CI <travis@travis-ci.org>
2019-09-24 22:26:03 +0000

    Deploy code docs to GitHub Pages Travis build: 216
    Commit: ed1bfe5a61d0950d4f37601492ad2f7f3c007c65

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['adafruit_5farcada_5fspitft',['Adafruit_Arcada_SPITFT',['../class_adafruit___arcada___s_p_i_t_f_t.html',1,'Adafruit_Arcada_SPITFT'],['../class_adafruit___arcada___s_p_i_t_f_t.html#aed36639ae8ee400dcd62591b536670de',1,'Adafruit_Arcada_SPITFT::Adafruit_Arcada_SPITFT()']]],
   ['alertbox',['alertBox',['../class_adafruit___arcada___s_p_i_t_f_t.html#aa4ab82ab00330377076d7665ed947966',1,'Adafruit_Arcada_SPITFT']]],
-  ['arcadabegin',['arcadaBegin',['../class_adafruit___arcada___s_p_i_t_f_t.html#a3a78e22b6b1d224b09567e7cfe7f9242',1,'Adafruit_Arcada_SPITFT']]]
+  ['arcadabegin',['arcadaBegin',['../class_adafruit___arcada___s_p_i_t_f_t.html#a3a78e22b6b1d224b09567e7cfe7f9242',1,'Adafruit_Arcada_SPITFT']]],
+  ['availableflash',['availableFlash',['../class_adafruit___arcada___s_p_i_t_f_t.html#aeab1a5c507cb0705be2fe1e3106e9ff5',1,'Adafruit_Arcada_SPITFT']]]
 ];
--- html/search/functions_12.js
+++ html/search/functions_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['warnbox',['warnBox',['../class_adafruit___arcada___s_p_i_t_f_t.html#a30dbebbf4f2b2853d93fa657eb055423',1,'Adafruit_Arcada_SPITFT']]],
-  ['writefiletoflash',['writeFileToFlash',['../class_adafruit___arcada___s_p_i_t_f_t.html#a1230de148048fb511f5d51a1fdd04cd8',1,'Adafruit_Arcada_SPITFT']]]
+  ['writedatatoflash',['writeDataToFlash',['../class_adafruit___arcada___s_p_i_t_f_t.html#a30942f35600713a096d785854081744c',1,'Adafruit_Arcada_SPITFT']]],
+  ['writefiletoflash',['writeFileToFlash',['../class_adafruit___arcada___s_p_i_t_f_t.html#a37b0b7ea67cda40b5758fb5174153940',1,'Adafruit_Arcada_SPITFT']]]
 ];
