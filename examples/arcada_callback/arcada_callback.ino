92b899f67747000d0d758b8c5bc47acb288b6374
ladyada <limor@ladyada.net>
2019-10-04 20:11:11 -0400

    callback demo

---
+++ examples/arcada_callback/arcada_callback.ino
@@ -0,0 +1,32 @@
+#include "Adafruit_Arcada.h"
+Adafruit_Arcada arcada;
+
+
+volatile bool togglepin = false;
+void mycallback(void) {
+  digitalWrite(LED_BUILTIN, togglepin);
+  togglepin = ! togglepin;
+}
+
+void setup() {
+  Serial.begin(115200);
+  //while (!Serial) delay(10);
+  Serial.print("Hello! Arcada Callback Test");
+
+  uint8_t i =0;
+
+  // Start TFT and fill black
+  if (!arcada.arcadaBegin()) {
+    Serial.print("Failed to begin");
+    while (1);
+  }
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // make a 1KHz callback
+  arcada.timerCallback(1000, mycallback);
+}
+
+void loop() {
+  // nothing happens here!
+}
