f25dae67f0c58df40eb16f73bdd522f8fc3a0d54
Travis CI <travis@travis-ci.org>
2019-10-06 02:55:00 +0000

    Deploy code docs to GitHub Pages Travis build: 240
    Commit: 4d111afc22dbc8656b2d869d8e4920feaea614c9

--- html/search/functions_10.js
+++ html/search/functions_10.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['timercallback',['timerCallback',['../class_adafruit___arcada___s_p_i_t_f_t.html#a81d3317b5c5d17a4a0fc26f7dab60a17',1,'Adafruit_Arcada_SPITFT']]]
+  ['timercallback',['timerCallback',['../class_adafruit___arcada___s_p_i_t_f_t.html#a81d3317b5c5d17a4a0fc26f7dab60a17',1,'Adafruit_Arcada_SPITFT']]],
+  ['timerstop',['timerStop',['../class_adafruit___arcada___s_p_i_t_f_t.html#a861311f37b32cdc81f8595060bc5473f',1,'Adafruit_Arcada_SPITFT']]]
 ];
