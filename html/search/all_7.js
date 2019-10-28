d6fd9b8d87caf34d1b7d23093534d031714a6908
Travis CI <travis@travis-ci.org>
2019-10-28 02:36:25 +0000

    Deploy code docs to GitHub Pages Travis build: 269
    Commit: a0dbb94aeb00acb5d9b5bcdb4c6be8029e07ab59

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -4,6 +4,8 @@ var searchData=
   ['getcanvas',['getCanvas',['../class_adafruit___arcada___s_p_i_t_f_t.html#a5368e7d0b9ec18a05df66f34be7be8e4',1,'Adafruit_Arcada_SPITFT']]],
   ['getframebuffer',['getFrameBuffer',['../class_adafruit___arcada___s_p_i_t_f_t.html#a02b2621db86c94e0bd0cf14525e66600',1,'Adafruit_Arcada_SPITFT']]],
   ['getimagereader',['getImageReader',['../class_adafruit___arcada___s_p_i_t_f_t.html#ad21603d22754e29b42b93d1c4ae48388',1,'Adafruit_Arcada_SPITFT']]],
+  ['gettimercallback',['getTimerCallback',['../class_adafruit___arcada___s_p_i_t_f_t.html#a3c09345939150f2e5dc0d81e2f23b0dd',1,'Adafruit_Arcada_SPITFT']]],
+  ['gettimercallbackfreq',['getTimerCallbackFreq',['../class_adafruit___arcada___s_p_i_t_f_t.html#a9df1d10b0b9c720a493ae06f0035bce0',1,'Adafruit_Arcada_SPITFT']]],
   ['gettouchscreenpoint',['getTouchscreenPoint',['../class_adafruit___arcada___s_p_i_t_f_t.html#ab1260b72f857a2bb95b49fc82c1e66fd',1,'Adafruit_Arcada_SPITFT']]],
   ['getvolume',['getVolume',['../class_adafruit___arcada___s_p_i_t_f_t.html#a5c88fe786f882f3e096aa9b20d75025e',1,'Adafruit_Arcada_SPITFT']]]
 ];
--- html/search/functions_12.js
+++ html/search/functions_12.js
@@ -1,6 +1,10 @@
 var searchData=
 [
   ['warnbox',['warnBox',['../class_adafruit___arcada___s_p_i_t_f_t.html#a30dbebbf4f2b2853d93fa657eb055423',1,'Adafruit_Arcada_SPITFT']]],
+  ['wavload',['WavLoad',['../class_adafruit___arcada___s_p_i_t_f_t.html#aca8c278547b8713a5f6cc653075b7180',1,'Adafruit_Arcada_SPITFT::WavLoad(char *filename, uint32_t *samplerate)'],['../class_adafruit___arcada___s_p_i_t_f_t.html#a8ef86a21729313bce06dadcdf05b0326',1,'Adafruit_Arcada_SPITFT::WavLoad(File f, uint32_t *samplerate)']]],
+  ['wavplaynextsample',['WavPlayNextSample',['../class_adafruit___arcada___s_p_i_t_f_t.html#aac305d2d90649e87f0a8b582f43bc39c',1,'Adafruit_Arcada_SPITFT']]],
+  ['wavreadfile',['WavReadFile',['../class_adafruit___arcada___s_p_i_t_f_t.html#a24f347c31864c43cedb3d3b9ebe613dc',1,'Adafruit_Arcada_SPITFT']]],
+  ['wavreadyfordata',['WavReadyForData',['../class_adafruit___arcada___s_p_i_t_f_t.html#a000e75f6b30f1e214fdc2f40d1ff48dc',1,'Adafruit_Arcada_SPITFT']]],
   ['writedatatoflash',['writeDataToFlash',['../class_adafruit___arcada___s_p_i_t_f_t.html#a30942f35600713a096d785854081744c',1,'Adafruit_Arcada_SPITFT']]],
   ['writefiletoflash',['writeFileToFlash',['../class_adafruit___arcada___s_p_i_t_f_t.html#a37b0b7ea67cda40b5758fb5174153940',1,'Adafruit_Arcada_SPITFT']]]
 ];
