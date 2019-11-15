6fd524c58fa0bab460c93fe0cfb9741a981aca1e
Travis CI <travis@travis-ci.org>
2019-11-15 02:25:07 +0000

    Deploy code docs to GitHub Pages Travis build: 289
    Commit: 34b5c1f3cc2391ed4d3ed417c657112e4e7155f4

--- html/search/functions_12.js
+++ html/search/functions_12.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['warnbox',['warnBox',['../class_adafruit___arcada___s_p_i_t_f_t.html#a30dbebbf4f2b2853d93fa657eb055423',1,'Adafruit_Arcada_SPITFT']]],
   ['wavload',['WavLoad',['../class_adafruit___arcada___s_p_i_t_f_t.html#aca8c278547b8713a5f6cc653075b7180',1,'Adafruit_Arcada_SPITFT::WavLoad(char *filename, uint32_t *samplerate)'],['../class_adafruit___arcada___s_p_i_t_f_t.html#a8ef86a21729313bce06dadcdf05b0326',1,'Adafruit_Arcada_SPITFT::WavLoad(File f, uint32_t *samplerate)']]],
+  ['wavplaycomplete',['WavPlayComplete',['../class_adafruit___arcada___s_p_i_t_f_t.html#a1835c40f438fd38e850c1ced42cf1301',1,'Adafruit_Arcada_SPITFT::WavPlayComplete(char *filename)'],['../class_adafruit___arcada___s_p_i_t_f_t.html#ade81c30ef31859638c01bfc87abcf867',1,'Adafruit_Arcada_SPITFT::WavPlayComplete(File f)']]],
   ['wavplaynextsample',['WavPlayNextSample',['../class_adafruit___arcada___s_p_i_t_f_t.html#aac305d2d90649e87f0a8b582f43bc39c',1,'Adafruit_Arcada_SPITFT']]],
   ['wavreadfile',['WavReadFile',['../class_adafruit___arcada___s_p_i_t_f_t.html#a24f347c31864c43cedb3d3b9ebe613dc',1,'Adafruit_Arcada_SPITFT']]],
   ['wavreadyfordata',['WavReadyForData',['../class_adafruit___arcada___s_p_i_t_f_t.html#a000e75f6b30f1e214fdc2f40d1ff48dc',1,'Adafruit_Arcada_SPITFT']]],
